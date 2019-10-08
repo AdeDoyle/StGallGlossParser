b982edac528c18e78684c4638aae51d0c0380472
Adrian <admd911@hotmail.com>
2019-10-08 16:06:21 +0100

    Clean_ExcelLists.py updated to to enable control over removal/replacement of hyphens, ellipses, and upper-case characters. Full stop character removal simplified.

--- Clean_Glosses.py
+++ Clean_Glosses.py
@@ -10,7 +10,7 @@ testlist = list()
 
 
 @lru_cache(maxsize=3500)
-def clean_gloss(gloss, lowercase=False):
+def clean_gloss(gloss, lowercase=False, ellipses=True, rem_hyphen=False, rem_greek=False):
     """Takes a gloss and removes all instances of tags or undesirable characters within the gloss"""
     # removes all unnecessary tags and tag content
     if lowercase:
@@ -60,12 +60,16 @@ def clean_gloss(gloss, lowercase=False):
     for rem in remset:
         if rem in gloss:
             gloss = "".join(gloss.split(rem))
-    # if "-" in gloss:
-    #     gloss = " ".join(gloss.split("-"))
-    # greekpat = re.compile(r'\.?[αιλμοπρςστυω]+\.?')
-    # greekpatitir = greekpat.finditer(gloss)
-    # for i in greekpatitir:
-    #     gloss = "".join(gloss.split(i.group()))
+    # remove hyphens if explicitly stated as function argument
+    if rem_hyphen:
+        if "-" in gloss:
+            gloss = " ".join(gloss.split("-"))
+    # remove greek characters if explicitly stated as function argument
+    if rem_greek:
+        greekpat = re.compile(r'\.?[αιλμοπρςστυω]+\.?')
+        greekpatitir = greekpat.finditer(gloss)
+        for i in greekpatitir:
+            gloss = "".join(gloss.split(i.group()))
     bracpat = re.compile(r'\(cf\. .*\)')
     bracpatitir = bracpat.finditer(gloss)
     for i in bracpatitir:
@@ -94,84 +98,21 @@ def clean_gloss(gloss, lowercase=False):
     numpatitir = numpat.finditer(gloss)
     for i in numpatitir:
         gloss = "".join(gloss.split(i.group()))
-    # removes unwanted full stops
-    abbrevlist = [".i.", " .i.", ".i. ", " .i. "]
-    abbrevpat = re.compile(r' \.[a-záéíóúA-ZÁÉÍÓÚαιλμοπρςστυω]+\. ')
-    # abbrevpat = re.compile(r' \.[a-záéíóúA-ZÁÉÍÓÚ]+\. ')
-    abbrevpatitir = abbrevpat.finditer(gloss)
-    for i in abbrevpatitir:
-        abbrev = i.group()
-        if abbrev not in abbrevlist:
-            abbrev = "".join(abbrev.split("."))
-            abbrev = abbrev.strip()
-            gloss = abbrev.join(gloss.split(".{}.".format(abbrev)))
-    # must be run twice for instances like " .is. .m. " in gloss 136a27 (10025-10040, in SG. Combined Data)
-    abbrevpatitir = abbrevpat.finditer(gloss)
-    for i in abbrevpatitir:
-        abbrev = i.group()
-        if abbrev not in abbrevlist:
-            abbrev = "".join(abbrev.split("."))
-            abbrev = abbrev.strip()
-            gloss = abbrev.join(gloss.split(".{}.".format(abbrev)))
-    abbrevpat = re.compile(r' \.[a-záéíóúA-ZÁÉÍÓÚαιλμοπρςστυω]+\.$')
-    # abbrevpat = re.compile(r' \.[a-záéíóúA-ZÁÉÍÓÚ]+\.$')
-    abbrevpatitir = abbrevpat.finditer(gloss)
-    for i in abbrevpatitir:
-        abbrev = i.group()
-        if abbrev not in abbrevlist:
-            abbrev = "".join(abbrev.split("."))
-            abbrev = abbrev.strip()
-            gloss = abbrev.join(gloss.split(".{}.".format(abbrev)))
-    abbrevpat = re.compile(r'^\.[a-záéíóúA-ZÁÉÍÓÚαιλμοπρςστυω]+\. ')
-    # abbrevpat = re.compile(r'^\.[a-záéíóúA-ZÁÉÍÓÚ]+\. ')
-    abbrevpatitir = abbrevpat.finditer(gloss)
-    for i in abbrevpatitir:
-        abbrev = i.group()
-        if abbrev not in abbrevlist:
-            abbrev = "".join(abbrev.split("."))
-            abbrev = abbrev.strip()
-            gloss = abbrev.join(gloss.split(".{}.".format(abbrev)))
-    fspat = re.compile(r'.{0,5}[^.][^i\.]\.$')
-    fspatitir = fspat.finditer(gloss.strip())
-    for i in fspatitir:
-        stopstring = i.group()
-        nostopstring = stopstring[:stopstring.rfind(".")] + stopstring[stopstring.rfind(".") + 1:]
-        gloss = nostopstring.join(gloss.split(stopstring))
-    fspat = re.compile(r'^\.+[^i][^.].{0,5}')
-    fspatitir = fspat.finditer(gloss.strip())
-    for _ in fspatitir:
-        gloss = gloss[:gloss.find(".")] + gloss[gloss.find(".") + 1:]
-
-    fspat = re.compile(r' \.\w+$')
-    fspatitir = fspat.finditer(gloss)
-    for i in fspatitir:
-        stopstring = i.group()
-        nostopstring = "".join(stopstring.split("."))
-        gloss = nostopstring.join(gloss.split(stopstring))
-    fspat = re.compile(r' \.\w+ ')
-    fspatitir = fspat.finditer(gloss)
-    for i in fspatitir:
-        stopstring = i.group()
-        nostopstring = "".join(stopstring.split("."))
-        gloss = nostopstring.join(gloss.split(stopstring))
-    # fspat = re.compile(r'\.[^i \.]\w*')
-    # fspatitir = fspat.finditer(gloss)
-    # for i in fspatitir:
-    #     print(i.group())
-    #     print(gloss)
-    # fspat = re.compile(r'\w\.\w')
-    # fspatitir = fspat.finditer(gloss)
-    # for i in fspatitir:
-    #     print(i.group())
-    #     print(gloss)
-    fspat = re.compile(r'[^\.]\.\w{2,}\.')
-    fspatitir = fspat.finditer(gloss)
-    for i in fspatitir:
-        stopstring = i.group()
-        nostopstring = "".join(stopstring.split("."))
-        gloss = nostopstring.join(gloss.split(stopstring))
-    
-
+    # removes/replaces unwanted full stops with ellipses or blank spaces as required
+    ellipat = re.compile(r'(?<!\.i)\.[ \.]*\.(?!i\.)')
+    ellipatitir = ellipat.finditer(gloss)
+    if ellipses:
+        elreplace = "…"
+    else:
+        elreplace = ""
+    for _ in ellipatitir:
+        gloss = ellipat.sub(elreplace, gloss)
+        break
+    stopat = re.compile(r'(?<!\.i)\.(?!i\.)')
+    stopatitir = stopat.finditer(gloss)
+    for _ in stopatitir:
+        gloss = stopat.sub("", gloss)
+        break
     # replace incorrect characters with appropriate alternatives
     if "7" in gloss:
         gloss = "⁊".join(gloss.split("7"))
@@ -198,27 +139,28 @@ def clean_gloss(gloss, lowercase=False):
     return gloss
 
 
-# Pick spreadsheet to draw glosses from (for all testing)
-sgData = list_xlsx("SG. Combined Data", "Sheet 1")
+# # Pick spreadsheet to draw glosses from (for all testing)
+# sgData = list_xlsx("SG. Combined Data", "Sheet 1")
 
 # # Print a single cleaned gloss from the combined data spreadsheet
 # testgloss = sgData[10025]
 # print(clean_gloss(testgloss[8]))
 
-# Clean all glosses in the SG. corpus, print each gloss (original, then clean)
-# Count all unique (cleaned) glosses
-lastgloss = ""
-glcount = 0
-cleaned_glosses = list()
-for i in sgData:
-    thisgloss = clean_gloss(i[8])
-    if thisgloss != lastgloss:
-        cleaned_glosses.append(thisgloss)
-        # print(i[8])
-        # print("{}: {}".format(i[0], thisgloss))
-        lastgloss = thisgloss
-        glcount += 1
-print(glcount)
+# # Clean all glosses in the SG. corpus, print each gloss (original, then clean)
+# # Count all unique (cleaned) glosses
+# lastgloss = ""
+# glcount = 0
+# cleaned_glosses = list()
+# for i in sgData:
+#     # thisgloss = clean_gloss(i[8], lowercase=True, rem_greek=True, rem_hyphen=True)
+#     thisgloss = clean_gloss(i[8])
+#     if thisgloss != lastgloss:
+#         cleaned_glosses.append(thisgloss)
+#         # print(i[8])
+#         # print("{}: {}".format(i[0], thisgloss))
+#         lastgloss = thisgloss
+#         glcount += 1
+# print(glcount)
 
 # # Print a sorted list of all characters in the cleaned St. Gall corpus
 # all_chars = list(set("".join(cleaned_glosses)))
